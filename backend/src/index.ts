1438e931844ba0091ec95e4a399f8e50c4b5a851
Harith A Kareem <harithakareem72@gmail.com>
2025-03-31 10:59:27 +0100

    fixed prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+import express from 'express'
+
+const app = express()
+
+app.get('/main', (req, res) => {
+  res.send('Main Page')
+})
+
+app.listen(3000, () => {
+  console.info('server is running on http://localhost:3000')
+})
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,11 @@
 export const App = () => {
-  const arr: number[] = [];
+  const arr: number[] = []
   return (
     <div>
-      <h1>Hello World</h1>{" "}
+      <h1>Hello World</h1>{' '}
       {arr.map((el) => (
         <h1>{el}</h1>
       ))}
     </div>
-  );
-};
+  )
+}
